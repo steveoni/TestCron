1018ddbaf2ca3886af0778fb9e9ba91b2ad7cadc
steveoni <steohenoni2@gmail.com>
2022-05-25 13:45:55 +0100

    test data flow automation

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-if __name__ == "__main__":
-    print("Tesing github schedule")
+from dataflows import Flow, dump_to_path
 
+data = [
+  {'data': 'Hello'},
+  {'data': 'World'}
+]
+
+def lowerData(row):
+    row['data'] = row['data'].lower()
+    
+
+def lowerme(row):
+    print(row)
+
+
+if __name__ == "__main__":
+    Flow(
+        data,
+        lowerData,
+        dump_to_path("data3/test")
+    ).process()
